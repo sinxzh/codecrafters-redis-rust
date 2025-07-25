d15e55e6b69fe5d186c897c5a5eb6522f67da7bf
sinxzh <sinxzh@outlook.com>
2025-07-25 17:56:51 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,62 @@
+use std::collections::HashMap;
 use std::io::{prelude::*, BufReader, BufWriter};
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-#[derive(Debug)]
-enum Command {
-    NULL,
-    PING,
-    ECHO,
+struct ResponseWriter<'a> {
+    writer: BufWriter<&'a TcpStream>,
 }
 
-struct Element {
-    cmd: Command,
-    key: Option<String>,
-    val: Option<String>,
-}
-
-impl Element {
-    fn new() -> Element {
-        Element {
-            cmd: Command::NULL,
-            key: None,
-            val: None,
+impl<'a> ResponseWriter<'a> {
+    fn new(stream: &'a TcpStream) -> ResponseWriter {
+        ResponseWriter {
+            writer: BufWriter::new(&stream),
         }
+    }
+
+    fn write_simple_string(&mut self, content: &str) {
+        self.writer.write_all(b"+").unwrap();
+        self.writer.write_all(content.as_bytes()).unwrap();
+        self.writer.write_all(b"\r\n").unwrap();
+        self.writer.flush().unwrap();
+    }
+
+    fn write_bulk_string(&mut self, content: &str) {
+        let str_len = format!("${}\r\n", content.len());
+        self.writer.write_all(str_len.as_bytes()).unwrap();
+
+        self.writer.write_all(content.as_bytes()).unwrap();
+        self.writer.write_all(b"\r\n").unwrap();
+        self.writer.flush().unwrap();
+    }
+
+    fn write_null_bulk_string(&mut self) {
+        self.writer.write_all(b"$-1\r\n").unwrap();
+        self.writer.flush().unwrap();
     }
 }
 
 struct Request<'a> {
     cnt: usize,
-    eles: Vec<Element>,
 
     buf_reader: BufReader<&'a TcpStream>,
-    buf_writer: BufWriter<&'a TcpStream>,
     buf: String,
+
+    resp_writer: ResponseWriter<'a>,
+
+    kvs: Arc<Mutex<HashMap<String, String>>>,
 }
 
 impl<'a> Request<'a> {
-    fn new(stream: &'a TcpStream) -> Request<'a> {
+    fn new(stream: &'a TcpStream, kvs: Arc<Mutex<HashMap<String, String>>>) -> Request<'a> {
         Request {
             cnt: 0,
-            eles: Vec::new(),
             buf_reader: BufReader::new(&stream),
-            buf_writer: BufWriter::new(&stream),
             buf: String::new(),
+            resp_writer: ResponseWriter::new(&stream),
+            kvs,
         }
-    }
-
-    fn write_simple_string(&mut self, content: &str) {
-        self.buf_writer.write_all(b"+").unwrap();
-        self.buf_writer.write_all(content.as_bytes()).unwrap();
-        self.buf_writer.write_all(b"\r\n").unwrap();
-        self.buf_writer.flush().unwrap();
-    }
-
-    fn write_bulk_string(&mut self) {
-        let str_len = format!("${}\r\n", self.buf.trim().len());
-        self.buf_writer.write_all(str_len.as_bytes()).unwrap();
-
-        self.buf_writer.write_all(self.buf.as_bytes()).unwrap();
-        self.buf_writer.flush().unwrap();
     }
 
     fn read_line(&mut self) -> usize {
@@ -79,32 +78,6 @@ impl<'a> Request<'a> {
         self.cnt -= 1;
     }
 
-    fn read_command(&mut self) {
-        assert!(self.read_line() > 0);
-        assert!(self.buf.starts_with('$'));
-
-        assert!(self.read_line() > 0);
-
-        let mut ele = Element::new();
-        match self.buf.trim().to_uppercase().as_str() {
-            "PING" => {
-                ele.cmd = Command::PING;
-            }
-            "ECHO" => {
-                ele.cmd = Command::ECHO;
-
-                self.read_bulk_string();
-                ele.val = Some(self.buf.trim().to_string());
-            }
-            _ => {
-                panic!("read unknown command: {}", &self.buf);
-            }
-        }
-
-        self.eles.push(ele);
-        self.cnt -= 1;
-    }
-
     fn parse(&mut self) {
         loop {
             let n = self.read_line();
@@ -116,53 +89,88 @@ impl<'a> Request<'a> {
             assert!(self.buf.starts_with('*'));
             self.cnt = self.buf[1..].trim().parse().unwrap();
 
-            let mut cmd_idx = 0;
             while self.cnt > 0 {
-                self.read_command();
-                self.parse_command(cmd_idx);
-                cmd_idx += 1;
+                self.parse_command();
             }
         }
     }
 
-    fn parse_command(&mut self, idx: usize) {
-        let ele = &self.eles[idx];
-        println!("parse command: {:?}", ele.cmd);
+    fn parse_command(&mut self) {
+        assert!(self.read_line() > 0);
+        assert!(self.buf.starts_with('$'));
 
-        match ele.cmd {
-            Command::PING => {
-                self.write_simple_string("PONG");
+        assert!(self.read_line() > 0);
+        let cmd = self.buf.trim().to_uppercase();
+        println!("parse command: {}", cmd);
+
+        match cmd.as_str() {
+            "PING" => {
+                self.resp_writer.write_simple_string("PONG");
             }
-            Command::ECHO => {
-                self.write_bulk_string();
+            "ECHO" => {
+                self.read_bulk_string();
+                let val = &self.buf.trim().to_string();
+                self.resp_writer.write_bulk_string(val.as_str());
+            }
+            "SET" => {
+                self.read_bulk_string();
+                let key = self.buf.trim().to_string();
+
+                self.read_bulk_string();
+                let val = self.buf.trim().to_string();
+
+                self.kvs.lock().unwrap().insert(key, val);
+
+                self.resp_writer.write_simple_string("OK");
+            }
+            "GET" => {
+                self.read_bulk_string();
+                let key = self.buf.trim();
+
+                if let Some(val) = self.kvs.lock().unwrap().get(key) {
+                    self.resp_writer.write_bulk_string(val);
+                } else {
+                    self.resp_writer.write_null_bulk_string();
+                }
             }
             _ => {
-                panic!("parse unknown command");
+                panic!("read unknown command: {}", &self.buf);
             }
         }
+
+        self.cnt -= 1;
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(stream: TcpStream, kvs: Arc<Mutex<HashMap<String, String>>>) {
     println!("accepted new connection");
 
-    let mut req = Request::new(&stream);
+    let mut req = Request::new(&stream, kvs);
     req.parse();
 }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+    let mut handles = vec![];
+    let kvs: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
+
     for stream in listener.incoming() {
+        let kvs = Arc::clone(&kvs);
+
         match stream {
-            Ok(mut _stream) => {
-                thread::spawn(|| {
-                    handle_connection(_stream);
-                });
+            Ok(_stream) => {
+                handles.push(thread::spawn(move || {
+                    handle_connection(_stream, kvs);
+                }));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
